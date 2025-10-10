3789308833d7eac6b39f05094cc4393cd207c0b6
ahsanraza0786 <ahsanraza2924@gmail.com>
2025-10-10 13:32:52 +0530

    Added Google Authentication to login page

--- backend/controllers/googleController.js
+++ backend/controllers/googleController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("./models/User");
+const User = require("./models/user");
 const client = require("../google/googleClient");
 
 exports.googleLogin = async (req, res) => {
