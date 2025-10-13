fc603e56137ac9457c7b3bce67f63144e6dd2513
ahsanraza0786 <ahsanraza2924@gmail.com>
2025-10-13 14:42:14 +0530

    update the nextconfig file

--- frontend/vote/next.config.mjs
+++ frontend/vote/next.config.mjs
@@ -4,9 +4,21 @@
 // export default nextConfig;
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // If you have images from domains, add them here
+  images: {
+    domains: [], // leave empty if no external images
+  },
 };
 
-
+export default nextConfig;
